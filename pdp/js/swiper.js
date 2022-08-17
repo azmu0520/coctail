6f08ff1d5a9fef01eff93ddcb7466088b2db7338
azmu0520 <azizbekbootcam@gmail.com>
2022-08-17 14:14:38 +0500

    done

--- pdp/js/swiper.js
+++ pdp/js/swiper.js
@@ -9,13 +9,42 @@ var swiper = new Swiper('.swiperPdp', {
   },
 });
 
-var swiper = new Swiper('.swiperPdp2', {
+var swiper1 = new Swiper('.swiperPdp2', {
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
   },
   slidesPerView: 1,
 
+  breakpoints: {
+    800: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper2 = new Swiper('.simularPdp', {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: '.swiper-button-next-pdp',
+  },
+  loop: true,
+  breakpoints: {
+    1000: {
+      slidesPerView: 1.2,
+    },
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+var swiper3 = new Swiper('.simularPdp3', {
+  slidesPerView: 2,
+  navigation: {
+    nextEl: '.swiper-button-next-pdp3',
+    prevEl: '.swiper-button-prev-pdp3',
+  },
   breakpoints: {
     800: {
       slidesPerView: 3,
